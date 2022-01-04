91f8d378cd08334fc0f76b43b8f8af5f2647c2eb
Kirumipls <ana.ferreira.perso1@gmail.com>
2022-01-04 19:06:37 +0000

    Create zurraproducts_insertdata.sql

---
+++ zurraproducts_insertdata.sql
@@ -0,0 +1,32 @@
+USE ZurrapaSede;
+
+-- BEBIDAS
+INSERT INTO Products(id_product, name, category) VALUES (1, 'cafe', 'bebida');
+INSERT INTO Products(id_product, name, category) VALUES (2, 'agua 1.5L', 'bebida');
+INSERT INTO Products(id_product, name, category) VALUES (3, 'agua 0.33cl', 'bebida');
+INSERT INTO Products(id_product, name, category) VALUES (4, 'cerveja media', 'bebida');
+INSERT INTO Products(id_product, name, category) VALUES (5, 'cerveja mini', 'bebida');
+INSERT INTO Products(id_product, name, category) VALUES (6, 'moscatel', 'bebida');
+INSERT INTO Products(id_product, name, category) VALUES (7, 'refrigerante', 'bebida');
+INSERT INTO Products(id_product, name, category) VALUES (8, 'sumo', 'bebida');
+INSERT INTO Products(id_product, name, category) VALUES (9, 'bebida energetica', 'bebida');
+INSERT INTO Products(id_product, name, category) VALUES (10, 'cha', 'bebida');
+
+-- PASTELARIA
+INSERT INTO Products(id_product, name, category) VALUES (11, 'bolos diversos', 'pastelaria');
+INSERT INTO Products(id_product, name, category) VALUES (12, 'croissant', 'pastelaria');
+INSERT INTO Products(id_product, name, category) VALUES (13, 'pao', 'pastelaria');
+INSERT INTO Products(id_product, name, category) VALUES (14, 'pao de forma', 'pastelaria');
+INSERT INTO Products(id_product, name, category) VALUES (15, 'pao para cachorro', 'pastelaria');
+
+-- INGREDIENTES
+INSERT INTO Products(id_product, name, category) VALUES (16, 'fiambre', 'ingrediente');
+INSERT INTO Products(id_product, name, category) VALUES (17, 'queijo', 'ingrediente');
+INSERT INTO Products(id_product, name, category) VALUES (18, 'manteiga', 'ingrediente');
+INSERT INTO Products(id_product, name, category) VALUES (19, 'salsicha', 'ingrediente');
+INSERT INTO Products(id_product, name, category) VALUES (20, 'arroz', 'ingrediente');
+INSERT INTO Products(id_product, name, category) VALUES (21, 'carne', 'ingrediente');
+INSERT INTO Products(id_product, name, category) VALUES (22, 'batata frita', 'ingrediente');
+INSERT INTO Products(id_product, name, category) VALUES (23, 'alface', 'ingrediente');
+INSERT INTO Products(id_product, name, category) VALUES (24, 'tomate', 'ingrediente');
+INSERT INTO Products(id_product, name, category) VALUES (25, 'molhos', 'ingrediente');

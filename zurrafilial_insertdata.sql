3ae1e54a8f1e228a6950867770648d2ac63cb69b
Nuno Miguel Fernandes <55770319+nmfer@users.noreply.github.com>
2022-01-10 01:16:36 +0000

    Update zurrafilial_insertdata.sql

--- zurrafilial_insertdata.sql
+++ zurrafilial_insertdata.sql
@@ -1,5 +1,5 @@
 USE ZurrapaSede;
 
 -- FILIAIS
-INSERT INTO Branch(id_responsible, id_branch, designation, email, phone_num, address) VALUES (000001, 01, 'Filial 1', 'filial_1@gmail.com', 911567566, 'Rua Principal Lisboa n4');
-INSERT INTO Branch(id_responsible, ((SELECT TOP 1(id_branch) FROM Branch ORDER BY id_branch DESC) + 1), 'Filial 2', 'filial_2@gmail.com', 912345678, 'Rua Secundaria n21');
+INSERT INTO Branch(id_responsible, id_branch, designation, email, phone_num, address) VALUES (000001, 01, 'ZurrapaSede', 'sede@gmail.com', 911567566, 'n1 da Avenida Montes Hermínios, Covilhã');
+INSERT INTO Branch(id_responsible, ((SELECT TOP 1(id_branch) FROM Branch ORDER BY id_branch DESC) + 1), 'ZurrapaFilial 2', 'filial_2@gmail.com', 912345678, 'Rua Secundaria n21');
